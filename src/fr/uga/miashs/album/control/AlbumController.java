68ef287916b1d44b2f4cd55156aff845a1317da4
JulioJu <julioprayer0890@gmail.com>
2017-01-17 21:54:08 +0100

    Add, create album folder

--- src/fr/uga/miashs/album/control/AlbumController.java
+++ src/fr/uga/miashs/album/control/AlbumController.java
@@ -1,5 +1,10 @@
 package fr.uga.miashs.album.control;
 
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
@@ -17,32 +22,82 @@ public class AlbumController {
 
 	@Inject
 	private AppUserSession appUserSession;
-	
+
 	@Inject
 	private AlbumService albumService;
 
-	
 	private Album album;
-	
-	
+
 	public Album getAlbum() {
 		if (album==null) {
 			album = new Album(appUserSession.getConnectedUser());
 		}
 		return album;
 	}
-	
-	
+
 	public String createAlbum() {
+
+		// TODO a folder named ~/workspace/PhotoAlbumJavaEEPictures should be created
+		String homeDir = System.getProperty("user.home");
+		Path directory = Paths.get(homeDir,"/workspace/PhotoAlbumJavaEEPictures");
+		if (!Files.isDirectory(directory)) {
+			throw new FileSystemNotFoundException(directory + "does not exist or is not a directory.");
+		}
+
 		try {
 			albumService.create(album);
 		} catch (ServiceException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		Path albumDirectory = Paths.get(directory + "/" + album.getId());
+		if (Files.exists(albumDirectory)) {
+			albumService.deleteById(album.getId());
+			throw new FileSystemNotFoundException("Please delete '" + albumDirectory + "'. No action performed");
+		}
+		try {
+			Files.createDirectory(albumDirectory);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return Pages.list_album;
 	}
-	
+
+	// TODO EntityTransaction commit method is void. But how to check than commit
+	// was performed with success ? See JpaService.java
+	public String delete(long albumId) {
+
+		album = albumService.read(albumId);
+		if (album == null) {
+			throw new NullPointerException("The album with id " + albumId + " that you want to destroy not exists in database");
+		}
+
+		// TODO test with bool value
+		albumService.deleteById(albumId);
+
+		// TODO a folder named ~/workspace/PhotoAlbumJavaEEPictures should be created
+		String homeDir = System.getProperty("user.home");
+		Path directory = Paths.get(homeDir,"/workspace/PhotoAlbumJavaEEPictures");
+		if (!Files.isDirectory(directory)) {
+			throw new FileSystemNotFoundException("Root diretory '" + directory + "does not exist or is not a directory.");
+		}
+		Path albumDirectory = Paths.get(directory + "/" + album.getId());
+		if (!Files.isDirectory(albumDirectory)) {
+			throw new FileSystemNotFoundException("'" + albumDirectory + "' is not a directory. Fatal error.");
+		}
+		try {
+			Files.delete(albumDirectory);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		return Pages.list_album;
+	}
+
 	public List<Album> getListAlbumOwnedByCurrentUser() {
 		try {
 			return albumService.listAlbumOwnedBy(appUserSession.getConnectedUser());
@@ -53,3 +108,5 @@ public class AlbumController {
 		return null;
 	}
 }
+
+// vim: sw=4 ts=4 noet:
